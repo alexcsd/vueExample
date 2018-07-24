e40b52ccbd0e8c3b938ec7edcc3dc465a5d0f1c6
alexcsd <mohamedshabaan.sci2016@alexu.edu.eg>
2018-07-24 09:22:54 +0200

    vue-example

---
+++ src/components/FirstComponent.vue
@@ -0,0 +1,31 @@
+<template>
+    <div>
+        hi iam a component
+        {{myTitle}}
+        {{categories}}
+    </div>
+</template>
+
+<script>
+import axios from 'axios';
+export default {
+    props:['myTitle'],
+    data(){
+        return {
+            categories:''
+        }
+    },
+    created(){
+        axios.get('http://api.ar5ss.com/category')
+        .then(res => {
+            console.log(res);
+            this.categories=res.data;
+        })
+        .catch( e => console.log(e));
+    }
+}
+</script>
+
+<style>
+
+</style>
---
+++ src/components/pages/Home.vue
@@ -0,0 +1,16 @@
+<template>
+<div>
+    Home
+    <first-component />
+</div>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style>
+
+</style>
